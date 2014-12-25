de54eb3f899d3063f9be201de4691c2d51738613
Andrew Yang <ayang14@illinois.edu>
2014-12-25 17:08:53 -0600

    Successful port
    From front-end (HTML + CSS + JS) to back-end EJS templating with Node.js
    project directory structure

---
+++ public/javascripts/script.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+	// INITIALIZE JMENU
+	$("#navbar").jMenu();
+
+	// METHOD TO INITIALIZE EXTENSIONS' HEIGHTS AND POSITIONS
+	var setExtensions = function () {
+		// debugExtensions();
+		$(".navbar-extension").css({top : ($("#navbar").position().top + parseInt($("#navbar").css("margin-top"))) + "px"});
+		$(".navbar-extension").height($("#navbar").outerHeight());
+		$(".footer-extension").css({top : ($("#footer").position().top + parseInt($("#footer").css("margin-top"))) + "px"});
+		$(".footer-extension").height($("#footer").outerHeight());
+	};
+
+	debugExtensions = function () {
+		console.log("$('#navbar').position().top: " + $("#navbar").position().top);
+		console.log("$('#navbar').css('margin-top'): " + $("#navbar").css("margin-top"));
+		console.log("$('#navbar').height(): " + $("#navbar").height());
+		console.log("$('#navbar').outerHeight(): " + $("#navbar").outerHeight());
+		console.log("$('#footer').position().top: " + $("#footer").position().top);
+		console.log("$('#footer').css('margin-top'): " + $("#footer").css("margin-top"));
+		console.log("$('#footer').height(): " + $("#footer").height());
+		console.log("$('#footer').outerHeight(): " + $("#footer").outerHeight());
+
+		console.log("#navbar position: " + ($("#navbar").position().top + parseInt($("#navbar").css("margin-top"))));
+		console.log("#navbar height: " + $("#navbar").outerHeight());
+		console.log("#footer position: "  + ($("#footer").position().top + parseInt($("#footer").css("margin-top"))));
+		console.log("#footer height: " + $("#footer").outerHeight());
+
+		console.log(document.readyState);
+	};
+
+	// INITIALIZE EXTENSIONS' HEIGHTS AND POSITIONS AFTER DOM HAS LOADED PROPERLY
+	document.onreadystatechange = function () {
+		setExtensions();
+	};
+
+	// INITIALIZE SEARCH BAR
+	var defaultSearchValue = "Search SSS";
+	$("#search").val(defaultSearchValue);
+
+	$("#search").click(function() {
+		if ($("#search").val() == "Search SSS") {
+			$("#search").val("")
+		}
+	});
+
+	var changeCaption = function (element) {
+		//console.log(element);
+		var comment = element.find('img').attr('alt');
+        var title = element.find('img').attr('title');
+        if(comment && title) $('.imagecaption').html('<strong class="captiontitle">'+title+'</strong><p class="captiontext">'+comment+'</p>');
+	}
+
+	// INITIALIZE BUTTON
+	$("#subscribebutton").button();
+
+	// INITIALIZE JCAROUSEL
+	var carousel = $(".jcarousel").on("jcarousel:createend", function (event, carousel) {
+		// Force jCarousel to update caption
+		if (window.location.hash) {
+			//console.log('has hash');
+			var hash = window.location.hash.split('#')[1] - 1;
+			//console.log(carousel._target);
+			$(".jcarousel").jcarousel("scroll", hash);
+			changeCaption(carousel._target);
+		} else {
+			//console.log('no hash');
+			//console.log(carousel._first.find("img"));
+			$(".jcarousel").jcarousel("scroll", 0);
+			changeCaption(carousel._first);
+		}
+	}).jcarousel({
+		transitions : true,
+		center : true,
+		wrap : "both"
+	});
+
+	$('.jcarousel').on('jcarousel:targetin', 'li', function (event, carousel) {
+       	// console.log("Target in");
+       	var image = $(this);
+    	$(".imagecaption").animate({ left: "-=600px" }, 300, "swing", function () {
+    		// console.log("Swing left");
+    		changeCaption(image);
+    	});
+    });
+
+    $('.jcarousel').on('jcarousel:targetout', 'li', function (event, carousel) {
+    	// console.log("Target out");
+    	$(".imagecaption").animate({ left: "+=600px"}, 300, "swing", function () {
+    		// console.log("Swing right");
+    	})
+    })
+
+	$(".jcarousel-prev").on("jcarouselcontrol:active", function () {
+		$(this).removeClass("inactive");
+	}).on("jcarouselcontrol:inactive", function () {
+		$(this).addClass("inactive");
+	}).jcarouselControl({
+		target: "-=1",
+		carousel: carousel
+	});
+
+	$(".jcarousel-next").on("jcarouselcontrol:active", function () {
+		$(this).removeClass("inactive");
+	}).on("jcarouselcontrol:inactive", function () {
+		$(this).addClass("inactive");
+	}).jcarouselControl({
+		target: "+=1",
+		carousel: carousel
+	});
+
+	$(".jcarousel-pagination").on("jcarouselpagination:active", "a", function () {
+		$(this).addClass("active");
+	}).on("jcarouselpagination:inactive", "a", function () {
+		$(this).removeClass("active");
+	}).jcarouselPagination({
+		item: function (page) {
+			return '<a href="#' + page + '">' + page + '</a>';
+		},
+		carousel: carousel
+	});
+
+	$(".jcarousel").jcarouselAutoscroll({
+		target: "+=1",
+		interval: 5000,
+		create: $(".jcarousel").hover(function () {
+			$(this).jcarouselAutoscroll("stop");
+		}, function () {
+			$(this).jcarouselAutoscroll("start");
+		})
+	})
+
+	// END JCAROUSEL METHODS
+
+	
+});
